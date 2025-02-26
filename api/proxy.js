f70c0917d48d879686cfef4e555a5b2698f69b03
wikimodels <young.wiki.models@gmail.com>
2025-02-26 06:52:46 +0300

    test-shit-4

--- api/proxy.js
+++ api/proxy.js
@@ -11,6 +11,12 @@
 //   }
 // }
 export default async function handler(req, res) {
+  const bybitUrl =
+    "https://api.bybit.com/v5/market/kline?category=linear&symbol=BTCUSDT&interval=15&limit=200";
+
+  const response = await fetch(bybitUrl);
+  const data = await response.json();
+  console.log(data);
   console.log("Simple test endpoint hit");
   res.status(200).json({ message: "Hello, world!" });
 }
