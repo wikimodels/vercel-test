521e8a29f449645f4fcbead79bfc0b824fe987e4
wikimodels <young.wiki.models@gmail.com>
2025-02-26 06:49:34 +0300

    test shit-3

--- api/proxy.js
+++ api/proxy.js
@@ -1,12 +1,16 @@
+// export default async function handler(req, res) {
+//   const bybitUrl =
+//     "https://api.bybit.com/v5/market/kline?category=linear&symbol=BTCUSDT&interval=15&limit=200";
+//   try {
+//     const response = await fetch(bybitUrl);
+//     const data = await response.json();
+//     res.send({ data: "Fucking Shit" });
+//   } catch (error) {
+//     console.error("Proxy error:", error);
+//     res.status(500).json({ error: "WTF???" });
+//   }
+// }
 export default async function handler(req, res) {
-  const bybitUrl =
-    "https://api.bybit.com/v5/market/kline?category=linear&symbol=BTCUSDT&interval=15&limit=200";
-  try {
-    const response = await fetch(bybitUrl);
-    const data = await response.json();
-    res.send({ data: "Fucking Shit" });
-  } catch (error) {
-    console.error("Proxy error:", error);
-    res.status(500).json({ error: "WTF???" });
-  }
+  console.log("Simple test endpoint hit");
+  res.status(200).json({ message: "Hello, world!" });
 }
